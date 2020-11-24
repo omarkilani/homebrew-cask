4e80398491293e5b4bfbbe94b1a1ed465806e726
Markus Reiter <me@reitermark.us>
2020-11-24 19:42:21 +0100

    Try reproducing “bom contains no files, nothing copied” error.

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask "goodsync" do
   version "11.4.4.4"
-  sha256 "bdce46b95ec0db546ad3bd7b48698f0246ebbd00c715145c476e487d7c48ed0c"
+  sha256 :no_check
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast "https://www.goodsync.com/download?os=macos",
--- Casks/trainerroad.rb
+++ Casks/trainerroad.rb
@@ -1,6 +1,6 @@
 cask "trainerroad" do
   version "2020.47.110"
-  sha256 "090d2a13208dc583b1371c3c9865cd47f17c2bf2bba38518b29d63c6bff334ba"
+  sha256 :no_check
 
   # trainrdtrcmn01un1softw01.blob.core.windows.net/ was verified as official when first introduced to the cask
   url "https://trainrdtrcmn01un1softw01.blob.core.windows.net/installers/mac/v001/Production/TrainerRoad.dmg"
